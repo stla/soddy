7cdf70f19aa5053b7b4b2a6f3e0ffa3a2cf60dd1
stla <laurent_step@yahoo.fr>
2018-06-14 06:44:01 +0200

    hierarchical2 (frames)

---
+++ src/HierarchicalHexlet2.hs
@@ -0,0 +1,55 @@
+module HierarchicalHexlet2
+  where
+import           Linear             hiding (point, vector)
+import           Utils.Circumsphere
+
+fromV3 :: V3 Double -> Point
+fromV3 (V3 a b c) = (a, b, c)
+
+toV3 :: Point -> V3 Double
+toV3 (a, b, c) = V3 a b c
+
+inversion :: V3 Double -> Double -> V3 Double -> Point
+inversion point radius center =
+  fromV3 $ omega ^+^ center - k / quadrance vector *^ vector
+  where
+  omega = V3 (2*radius) 0 0
+  s = V3 (radius * 0.5) (radius * sqrt 3 / 2) 0
+  k = quadrance (s ^-^ omega)
+  vector = point ^-^ omega ^-^ center
+
+pointOnSphere :: Double -> Double -> Double -> V3 Double -> V3 Double
+pointOnSphere theta phi r center = r *^ point ^+^ center
+  where
+  point = V3 (cos theta * cos phi) (sin theta * cos phi) (sin phi)
+
+oneSphere :: V3 Double -> Double -> Double -> (Point,Double)
+oneSphere center radius beta = (c, r)
+  where
+  pt = center ^+^ V3 (radius * cos beta * 2 / 3) (radius * sin beta * 2 / 3) 0
+  thRadius = radius / 3
+  p1 = inversion (pointOnSphere 0 0 thRadius pt) radius center
+  p2 = inversion (pointOnSphere (pi/2) 0 thRadius pt) radius center
+  p3 = inversion (pointOnSphere pi 0 thRadius pt) radius center
+  p4 = inversion (pointOnSphere 0 (pi/2) thRadius pt) radius center
+  ((cx,cy,_),r) = circumsphere p1 p2 p3 p4
+  c = (cx - 4*radius, cy, 0)
+
+hexlet :: Int -> Bool -> (Point,Double) -> [(Point,Double)]
+hexlet frame clockwise (center,radius) = map (oneSphere center' radius) angles
+  where
+  center' = toV3 center
+  frame' = fromIntegral frame :: Double
+  shift = frame' * (if clockwise then -pi/90 else pi/90)
+  angles = map (+shift) [0.0, pi/3, 2*pi/3, pi, 4*pi/3, 5*pi/3]
+
+-- hexlets :: Int -> [(Point,Double)]
+-- hexlets n | n==1 = hexlet ((0, 0, 0), 1)
+--           | otherwise = concatMap hexlet (hexlets (n-1))
+
+hexlets :: Int -> Int -> Bool -> [(Point,Double)]
+hexlets n frame clockwise
+  | n==0 = [((0, 0, 0), 1)]
+  | otherwise = let nclockwise = not clockwise in
+                concatMap (hexlet frame nclockwise) 
+                          (hexlets (n-1) frame nclockwise)
---
+++ src/SoddyHierarchical2.hs
@@ -0,0 +1,137 @@
+module SoddyHierarchical2
+  where
+import           Data.IORef
+import           Graphics.Rendering.OpenGL.GL
+import           Graphics.UI.GLUT
+import           HierarchicalHexlet2
+
+white,black,red :: Color4 GLfloat
+white      = Color4    1    1    1    1
+black      = Color4    0    0    0    1
+red        = Color4    1    0    0    1
+
+data Context = Context
+    {
+      contextRot1    :: IORef GLfloat
+    , contextRot2    :: IORef GLfloat
+    , contextRot3    :: IORef GLfloat
+    , contextSpheres :: IORef [((Double,Double,Double),Double)]
+    }
+
+display :: Context -> IORef Double -> DisplayCallback
+display context zoom = do
+  clear [ColorBuffer, DepthBuffer]
+  spheres <- get (contextSpheres context)
+  zoom' <- get zoom
+  r1 <- get (contextRot1 context)
+  r2 <- get (contextRot2 context)
+  r3 <- get (contextRot3 context)
+  loadIdentity
+  (_, size) <- get viewport
+  resize zoom' size
+  rotate r1 $ Vector3 1 0 0
+  rotate r2 $ Vector3 0 1 0
+  rotate r3 $ Vector3 0 0 1
+  mapM_ (\(center,radius) -> preservingMatrix $ do
+                  translate (toVector3 center)
+                  materialDiffuse Front $= red
+                  renderObject Solid $ Sphere' radius 60 60)
+        spheres
+  swapBuffers
+  where
+    toVector3 (x,y,z) = Vector3 x y z
+
+resize :: Double -> Size -> IO ()
+resize zoom s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+  matrixMode $= Projection
+  loadIdentity
+  perspective 45.0 (w'/h') 1.0 100.0
+  lookAt (Vertex3 0 0 (-3+zoom)) (Vertex3 0 0 0) (Vector3 0 1 0)
+  matrixMode $= Modelview 0
+  where
+    w' = realToFrac w
+    h' = realToFrac h
+
+keyboard :: IORef GLfloat -> IORef GLfloat -> IORef GLfloat -- rotations
+         -> IORef GLdouble -- zoom
+         -> IORef Int -- depth
+         -> IORef Int -- "frame"
+         -> IORef [((Double,Double,Double),Double)]
+         -> KeyboardCallback
+keyboard rot1 rot2 rot3 zoom depth frame spheres c _ = do
+  case c of
+    'e' -> rot1 $~! subtract 2
+    'r' -> rot1 $~! (+2)
+    't' -> rot2 $~! subtract 2
+    'y' -> rot2 $~! (+2)
+    'u' -> rot3 $~! subtract 2
+    'i' -> rot3 $~! (+2)
+    'm' -> zoom $~! (+1)
+    'l' -> zoom $~! subtract 1
+    'h' -> do
+      depth $~! (+1)
+      depth' <- get depth
+      frame' <- get frame
+      writeIORef spheres (hexlets depth' frame' True)
+    'n' -> do
+      depth $~! (\n -> if n>0 then n-1 else n)
+      depth' <- get depth
+      frame' <- get frame
+      writeIORef spheres (hexlets depth' frame' True)
+    'g' -> do
+      frame $~! (+1)
+      frame' <- get frame
+      depth' <- get depth
+      writeIORef spheres (hexlets depth' frame' True)
+    'b' -> do
+      frame $~! subtract 1
+      frame' <- get frame
+      depth' <- get depth
+      writeIORef spheres (hexlets depth' frame' True)
+    'q' -> leaveMainLoop
+    _   -> return ()
+  postRedisplay Nothing
+
+main :: IO ()
+main = do
+  _ <- getArgsAndInitialize
+  _ <- createWindow "Soddy's hierarchical hexlet"
+  windowSize $= Size 500 500
+  initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
+  clearColor $= black
+  materialAmbient FrontAndBack $= black
+  lighting $= Enabled
+  light (Light 0) $= Enabled
+  position (Light 0) $= Vertex4 0 0 (-500) 1
+  ambient (Light 0) $= white
+  diffuse (Light 0) $= white
+  specular (Light 0) $= white
+  depthFunc $= Just Less
+  shadeModel $= Smooth
+  rot1 <- newIORef 0.0
+  rot2 <- newIORef 0.0
+  rot3 <- newIORef 0.0
+  zoom <- newIORef 0.0
+  let depth = 1
+      frame = 0
+      spheres = hexlets depth frame True
+  depth' <- newIORef depth
+  frame' <- newIORef frame
+  spheres' <- newIORef spheres
+  displayCallback $= display Context {contextRot1 = rot1,
+                                      contextRot2 = rot2,
+                                      contextRot3 = rot3,
+                                      contextSpheres = spheres'}
+                             zoom
+  reshapeCallback $= Just (resize 0)
+  keyboardCallback $= Just (keyboard rot1 rot2 rot3 zoom depth' frame' spheres')
+  idleCallback $= Nothing
+  putStrLn "*** Soddy's hierarchical hexlet ***\n\
+        \    To quit, press q.\n\
+        \    Scene rotation: e, r, t, y, u, i\n\
+        \    Zoom: l, m\n\
+        \    Increase/decrease depth: h,n\n\
+        \    Rotate hexlets: g,b\n\
+        \"
+  mainLoop
